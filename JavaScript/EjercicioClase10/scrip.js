56242dc65deee75c6e397f4f43d68197a8e44955
CarlosOReino <carlosreino94@gmail.com>
2024-10-17 20:43:02 +0200

    ejercicios clase 10

---
+++ JavaScript/EjercicioClase10/scrip.js
@@ -0,0 +1,2 @@
+/*Cuando se pinche sobre el primer enlace, se oculte su sección relacionada*/
+
---
+++ JavaScript/EjerciciosExtra/script.js
@@ -0,0 +1,8 @@
+/*Haz una función que obtenga el maximo numero de un array*/
+const array = [-1, 5, 25, 2];
+function devolver(array, pos, max) {
+    if( array == pos == max){
+        
+    }
+}
+console.log(devolver(array));
