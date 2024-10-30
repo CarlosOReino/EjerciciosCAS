a25a763d0aed9220834c9b2be6810361d03d089e
CarlosOReino <carlosreino94@gmail.com>
2024-10-30 21:17:28 +0100

    ejercicios 5-1

--- JavaScript/EjerciciosClase5/eje-2.js
+++ JavaScript/EjerciciosClase5/eje-2.js
@@ -3,15 +3,50 @@
 function primero(n) {
   let cifras;
   let entrada;
-  entrada = parseInt(prompt("Ingrese un número entero: "));
+  entrada = prompt("Ingrese un número entero: ");
   cifras = 0;
-  if (entrada == 0) {
+  if (isNaN(entrada)) {
     alert("Ingrese los números!");
   } else {
-    for (let i = 0; i < entrada.length; i++) {
-      entrada[i] += cifras;
-    }
+     entrada = entrada.toString();
+    let cifras = entrada.length;
+  
     alert("El número de cifras es: " + cifras);
+    return cifras;
   }
-  return cifras;
+  
+}
+
+//2.Una función que muestre al usuario una secuencia de _ (se debe construir la cadena de uno en uno), la cantidad de _ será solicitada al usuario
+
+function devolverCadena() {
+  let cantidad = prompt("Introduce la cantidad de _ para construir: ");
+  cantidad = parseInt(cantidad);
+  if (isNaN(cantidad) || cantidad <= 0) {
+    alert("Ingresa una cantidad válida!");
+    return
+  }
+  let cadena = "";
+  for (let i = 0; i < cantidad; i++) {
+    cadena += "_"
+    
+   
+  }
+  alert("La cadena es: " + cadena);
+}
+
+function devolverCadena2() {
+  let cantidad = prompt("Introduce la cantidad de _ para construir: ");
+  cantidad = parseInt(cantidad);
+  if (isNaN(cantidad) || cantidad <= 0) {
+    alert("Ingresa una cantidad válida!");
+    return
+  }
+  let cadena = "";
+  for (let i = 0; i < cantidad; i++) {
+    cadena += "*+_"
+    
+   
+  }
+  alert("La cadena es: " + cadena);
 }
